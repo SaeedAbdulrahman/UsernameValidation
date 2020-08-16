ce3375608f660dadd8b6be562b7b49ce3a821751
Saeed Ibrahim Abdulrahman <saeed.ibrahim.abdulrahman@gmail.com>
2020-08-16 20:31:55 +0400

    Update validate.py
    Fixed some bugs

--- validate.py
+++ validate.py
@@ -1,24 +1,24 @@
-def checkLength(str, start, end):
-  if len(str) <= end and len(str) >= start:
+def checkLength(text, start, end):
+  if len(text) <= end and len(text) >= start:
     return True
   else:
     return False
 
-def checkStartsWithLetter(str):
+def checkStartsWithLetter(text):
   for alphabet in "abcdefghijklmnopqrstuvwxyz":
-    if str[0].lower() == alphabet:
+    if text[0].lower() == alphabet:
       return True
   return False
   
-def containsLNU(str):
+def containsLNU(text):
   #Contains Letter, Number, Dash or Underscore (only)
   counter = 0
-  for letter in str:
+  for letter in text:
       for alphabet in "abcdefghijklmnopqrstuvwxyz":
         if letter.lower() == alphabet:
           counter+=1
       for number in range(0,10):
-        if letter == number:
+        if str(letter) == str(number):
           counter+=1
       if letter == '_' or letter == '-':
         counter+=1
@@ -27,17 +27,17 @@ def containsLNU(str):
       counter = 0
   return True
 
-def endsWithUnderScore(str):
-  if str[len(str)-1] == "_":
+def endsWithUnderScore(text):
+  if text[len(text)-1] == "_":
     return True
   else:
     return False
 
-def validateUsername(str):
-  if checkLength(str, 4, 25) and checkStartsWithLetter(str) and containsLNU(str) and not endsWithUnderScore(str):
-    return 'true'
+def validateUsername(text):
+  if checkLength(text, 4, 25) and checkStartsWithLetter(text) and containsLNU(text) and not endsWithUnderScore(text):
+    return True
   else:
-    return 'false'
+    return False
 
 while True:
 	print("Write the username you wish to validate")
